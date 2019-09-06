3c17ddff40fdd3a22d28b5f1ca16be5caa06b916
trungtran61 <trungtran61@gmail.com>
2019-09-06 09:11:16 -0700

    initial report

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Header, Icon } from 'semantic-ui-react'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    reportData: []
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:60127/api/reports/gettippingexceptions?fromDate=01/01/2019&toDate=12/31/2019')
+      .then(response => {
+        this.setState(
+          {
+            reportData: response.data
+          })
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header as='h2'>
+          <Icon name='users' circular />
+          <Header.Content>Tipping Exceptions</Header.Content>
+        </Header>
+        <table>
+          <tbody>
+            <tr>
+              <th>
+                Job Number
+          </th>
+              <th>
+                Service Provider
+          </th>
+              <th>
+                Job Rate
+          </th>
+              <th>
+                tip Amount
+          </th>
+              <th>
+                Date Completed
+          </th>
+            </tr>
+            {this.state.reportData.map((row: any) =>
+              <tr key={row.jobNumber}>
+                <td>{row.jobNumber}</td>
+                <td>{row.serviceProvider}</td>
+                <td>{row.jobRate}</td>
+                <td>{row.tipAmount}</td>
+                <td>{row.completionDate}</td>
+              </tr>
+            )
+            }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
